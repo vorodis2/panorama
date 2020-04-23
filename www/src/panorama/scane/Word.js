c4b317a8bcf643ddf4ebae9e5c6e18407d747694
vorodis2 <vorodis2@gmail.com>
2020-04-23 17:59:59 +0300

    1

--- www/src/panorama/scane/Word.js
+++ www/src/panorama/scane/Word.js
@@ -47,7 +47,8 @@ export class Word  {
     set index(value) {
         if (this._index != value) {
             this._index = value;
-            this.visi3D.utility.sky.link=this.confScane.array[value].src
+            if(this.confScane.array[value])this.visi3D.utility.sky.link=this.confScane.array[value].src
+            
                    
             
         }
